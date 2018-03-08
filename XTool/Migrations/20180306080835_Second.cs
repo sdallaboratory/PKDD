744573d2c8a3beb5b5812601d9dce15b3e748e93
Sergei Solovev <sergey.soloviev@inbox.ru>
2018-03-08 14:03:10 +0400

    Revert "shit"
    This reverts commit dcb8fb5e47dd97f0326c0a519feb2edd7541ecc7.

---
+++ XTool/Migrations/20180306080835_Second.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace XTool.Migrations
+{
+    public partial class Second : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ExpertId",
+                table: "Evaluations",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Evaluations_ExpertId",
+                table: "Evaluations",
+                column: "ExpertId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Evaluations_AspNetUsers_ExpertId",
+                table: "Evaluations",
+                column: "ExpertId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Evaluations_AspNetUsers_ExpertId",
+                table: "Evaluations");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Evaluations_ExpertId",
+                table: "Evaluations");
+
+            migrationBuilder.DropColumn(
+                name: "ExpertId",
+                table: "Evaluations");
+        }
+    }
+}
