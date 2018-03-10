c817f224106692dfba4221f6ed3f711eae874a4f
Sergei Solovev <sergey.soloviev@inbox.ru>
2018-03-10 22:59:12 +0400

    views

--- XTool/Controllers/HomeController.cs
+++ XTool/Controllers/HomeController.cs
@@ -10,6 +10,7 @@ using XTool.Models;
 using XTool.Data;
 using XTool.Models.ActorModels;
 using XTool.Data.Storage;
+using XTool.Models.Roles;
 
 namespace XTool.Controllers
 {
@@ -19,21 +20,36 @@ namespace XTool.Controllers
     {
         private readonly IStorage<int> _storage;
 
-        public HomeController(IStorage<int>storage) // тут поставить Сторадж
+        public HomeController(IStorage<int> storage) // тут поставить Сторадж
         {
             _storage = storage;
         }
 
         public IActionResult Actors()
         {
-            ViewBag.Actors = _storage.GetAll<Actor>().Take(3); // тут отсортировать по релевантности перед Take
+            var actors = _storage.GetAll<Actor>(); // тут отсортировать по релевантности перед Take
+            var actualActors = actors.OrderBy(a => 12).Take(3); // Вот эту троечку вынести в конфиг
+            foreach (Actor actor in actualActors)
+                LoadActor(actor);
+            ViewBag.ActualActors = actualActors;
+            ViewBag.Actors = actors.Take(20);
             return View();
         }
 
         public IActionResult Actor(int id)
         {
-            
             var actor = _storage.Get<Actor>(id);
+            LoadActor(actor);
+            ViewBag.Actor = actor;
+            return View();
+        }
+
+        /// <summary>
+        /// Выгружает из БД все поля для данного актора
+        /// </summary>
+        /// <param name="actor">актор</param>
+        private void LoadActor(Actor actor)
+        {
             foreach (var collection in _storage.Context.Entry(actor).Collections)
             {
                 collection.Load();
@@ -42,12 +58,6 @@ namespace XTool.Controllers
             {
                 _storage.Context.Entry(period).Collection(p => p.CareerEvents).Load();
             }
-            
-            var Photos = actor.Photos;
-            var Videos = actor.Videos;
-            ViewBag.Actor = _storage.Get<Actor>(id);
-            ViewBag.Count = _storage.Count<Actor>();
-            return View();
         }
 
         public IActionResult Error()
