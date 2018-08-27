cfdeedb0b6812acabe87cdda744611edd93d92f6
Sergei Solovev <sergey.soloviev@inbox.ru>
2018-08-27 12:15:27 +0300

    menu

--- Pkdd/pkdd-ng-client/src/app/app-module/components/top-menu/top-menu.component.ts
+++ Pkdd/pkdd-ng-client/src/app/app-module/components/top-menu/top-menu.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MenuService } from '../../services/menu.service';
+import { WindowService } from '../../services/window.service';
 
 @Component({
   selector: 'pkdd-top-menu',
@@ -9,8 +10,13 @@ import { MenuService } from '../../services/menu.service';
 export class TopMenuComponent implements OnInit {
 
   constructor(
-    public readonly menu: MenuService
-  ) { }
+    public readonly menu: MenuService,
+    public readonly win: WindowService
+  ) {
+    win.WindowResized.subscribe(size => {
+      // To change mode of displaying items
+    });
+  }
 
   ngOnInit() {
   }
