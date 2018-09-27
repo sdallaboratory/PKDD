991359d6fbbed64b59bd6270acbed7fdf77c15f8
roman.batin <roman.batin@qoollo.com>
2018-09-27 23:37:42 +0300

    test

--- Pkdd/pkdd-ng-client/src/app/persons/components/persons-list/persons-list.component.ts
+++ Pkdd/pkdd-ng-client/src/app/persons/components/persons-list/persons-list.component.ts
@@ -1,6 +1,4 @@
-import { Person } from './../../../models/entities/person';
 import { Component, OnInit } from '@angular/core';
-import { ContentBlock } from '../../../models/entities/content-block';
 
 @Component({
   selector: 'pkdd-persons-list',
@@ -8,9 +6,6 @@ import { ContentBlock } from '../../../models/entities/content-block';
   styleUrls: ['./persons-list.component.scss']
 })
 export class PersonsListComponent implements OnInit {
-
-  public persons: Person[];
-  public blocks: ContentBlock[];
 
   constructor(
   ) { }
